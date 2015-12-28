e2fadb686d670e4dfc361e944c9e989d685a7218
jlippold <jlippold@datacation.com>
2015-12-27 23:28:01 -0500

    large refactor

--- app.js
+++ app.js
@@ -1,29 +1,48 @@
-var Insteon = require('home-controller').Insteon;
-var hub = new Insteon();
-var request = require('request');
-var util = require('util');
-var motion = hub.motion("367fa8");
+var express = require('express');
+var async = require('async');
+var insteon = require('./api/insteon');
+var harmony = require('./api/harmony');
+var router = require('./router');
+var app = express();
 
-motion.on('motion', function() {
-  console.log('Motion detected at ' + (new Date()));
-	notifyXBMC();
+app.use('/', router);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	res.status(404);
+	// default to plain-text. send()
+	res.type('txt').send('Not found');
 });
 
-hub.connect("192.168.1.104", function() {
-  console.log("connected to hub");
-  notifyXBMC();
-});
-
-function notifyXBMC() {
-	var servers = [
-		"192.168.1.85",
-		"192.168.1.103"
-	];
-
-	var jsonrpc = '{"jsonrpc":"2.0","id":1,"method":"Addons.ExecuteAddon","params":{"addonid":"script.securitycam"}}';
-
-	servers.forEach(function(server) {
-		request.get(util.format("http://%s/jsonrpc?request=%s", server, jsonrpc));
+// error handlers
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: app.get('env') === 'development' ? err : {}
 	});
+});
 
-}
+
+async.auto({
+    insteonHub: function(next){
+    	insteon.register(function() {
+    		next();
+    	});
+    },
+    harmonyActivities: function(next){
+    	harmony.init(function() {
+    		next();
+    	});
+    },
+    express: ['insteonHub', 'harmonyActivities', function(next, results){
+        app.listen(3000);
+        console.log("web server is running");
+        next();
+    }],
+}, function(err, results) {
+	
+});
+
+
+module.exports = app;
